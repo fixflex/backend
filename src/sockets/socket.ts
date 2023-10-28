0abf752e87fa7ba7f336b83bdd0ac7a2a83beab3
Ahmed Mostafa <ahmed.elasiri1@gmail.com>
2023-10-28 08:45:58 +0200

    Refactor socket event handlers and add join room functionality
    This commit refactors the socket event handlers in the SocketService class and adds a new event handler for joining a chat room. The sendMessage function has been split into two separate functions, sendMessageClient and sendMessageTasker, to handle messages from different types of users. The joinRoom function has been modified to use the new join event instead of joinRoom. The code for joining a chat room has been moved to the join event handler. This commit also includes some commented out code for future authentication implementation.

--- src/sockets/socket.ts
+++ src/sockets/socket.ts
@@ -23,55 +23,70 @@ class SocketService {
   }
 
   public initializeSocket() {
-    try {
-      // check if the user is authenticated and get the user id from the token and add it to the socket
-      // this.io.use((socket: AuthSocket, next) => {
-      //   const token = socket.handshake.auth.token;
-      //   if (!token) {
-      //     return next(new Error('Authentication error'));
-      //   }
-      //   const decoded = jwt.verify(token, env.JWT_SECRET_KEY) as JwtPayload;
-      //   socket.userId = decoded.userId;
-      //   next();
-      // });
+    // check if the user is authenticated and get the user id from the token and add it to the socket
+    // this.io.use((socket: AuthSocket, next) => {
+    //   const token = socket.handshake.auth.token;
+    //   if (!token) {
+    //     return next(new Error('Authentication error'));
+    //   }
+    //   const decoded = jwt.verify(token, env.JWT_SECRET_KEY) as JwtPayload;
+    //   socket.userId = decoded.userId;
+    //   next();
+    // });
 
-      this.io.on('connection', (socket: AuthSocket) => {
-        socket.on('message', async (data: IMessage) => {
-          try {
-            // TODO
-            console.log(data.message);
-            let chatRoom = await ChatModel.findById(data.chatId);
-            // if !chatRoom throw error
-            if (!chatRoom) throw new Error('Chat room not found');
-            // if chatRoom.participants doesn't include the sender throw error
-            if (chatRoom.client !== data.sender && chatRoom.tasker !== data.sender) throw new Error('You are not a participant in this chat room');
+    this.io.on('connection', (socket: AuthSocket) => {
+      socket.on('message', async (data: IMessage) => {
+        try {
+          // TODO
+          console.log(data.message);
+          let chatRoom = await ChatModel.findById(data.chatId);
+          // if !chatRoom throw error
+          if (!chatRoom) throw new Error('Chat room not found');
+          // if chatRoom.participants doesn't include the sender throw error
+          if (chatRoom.client !== data.sender && chatRoom.tasker !== data.sender) throw new Error('You are not a participant in this chat room');
 
-            let message = await MessageModel.create(data);
-            if (!message) throw new Error('Message not created');
-            // push the message id to the messages array in the chat room and save it
-            chatRoom.messages.push(message._id);
-            await chatRoom.save();
+          let message = await MessageModel.create(data);
+          if (!message) throw new Error('Message not created');
+          // push the message id to the messages array in the chat room and save it
+          chatRoom.messages.push(message._id);
+          await chatRoom.save();
 
-            // emit the message to the tasker in the chat room
-            // broadcast to all clients in the chat room except the sender
-            // socket.broadcast.to(data.chatId!).emit('message', data);
-            // emit to all clients in the chat room
-            this.io.to(data.chatId!).emit('message', data);
-          } catch (error: any) {
-            console.log(error);
-            socket.emit('error', { message: error.message });
-          }
-        });
-
-        socket.on('disconnect', () => {
-          console.log('User disconnected from socket:', socket.id);
-        });
+          // emit the message to the tasker in the chat room
+          // broadcast to all clients in the chat room except the sender
+          socket.broadcast.to(data.chatId!).emit('message', data);
+          // emit to all clients in the chat room
+          // this.io.to(data.chatId!).emit('message', data);
+        } catch (error: any) {
+          console.log(error);
+          socket.emit('error', { message: error.message });
+        }
       });
 
-      // get connected users each 30 seconds
-    } catch (error: any) {
-      console.log(error);
-    }
+      // join user to chat room
+      socket.on('join', async (chatId: string) => {
+        try {
+          // TODO
+          // check if the user is a participant in the chat room
+          console.log(chatId, '###################################');
+          let chatRoom = await ChatModel.findById(chatId);
+          // if !chatRoom throw error
+          if (!chatRoom) throw new Error('Chat room not found');
+          // if chatRoom.participants doesn't include the sender throw error
+          // if (chatRoom.client !== socket.userId && chatRoom.tasker !== socket.userId) throw new Error('You are not a participant in this chat room');
+
+          socket.join(chatId);
+        } catch (error: any) {
+          console.log(error);
+          socket.emit('error', { message: error.message });
+        }
+      });
+
+      socket.on('disconnect', () => {
+        console.log('User disconnected from socket:', socket.id);
+      });
+    });
+
+    // get connected users each 30 seconds
   }
   // reutrn number of connected users
   public getConnectedUsers() {
