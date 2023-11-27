8841fd38b785e475ae0910270e4245fe100001f8
Ahmed Mostafa <ahmed.elasiri1@gmail.com>
2023-11-27 05:53:14 +0200

    Refactor auth.test.ts file
    - Refactored import paths in auth.test.ts file to match the updated file structure.
    - Updated the newUserData object to include firstName and lastName instead of name and username.
    - Removed the test case for checking if the username is already in use.
    - Updated the request body in the signup and login test cases to match the updated field names.
    - Updated the error message in the login test case to remove the mention of username.
    - Updated the request body in the fails login without required fields test case to match the updated field names.

--- __test__/integration/auth.test.ts
+++ __test__/integration/auth.test.ts
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 import request from 'supertest';
 
-import { server } from '../../';
-import User from '../../DB/models/user/client.model';
+import { server } from '../../src';
+import User from '../../src/DB/models/user/client.model';
 
 afterAll(async () => {
   await mongoose.connection.dropDatabase();
@@ -15,11 +15,10 @@ beforeEach(async () => {
 });
 
 const newUserData = {
-  name: 'John Doe',
+  firstName: 'John',
+  lastName: 'Doe',
   email: 'John@gmail.com',
-  username: 'JohnDoe',
   password: 'password123',
-  confirmPassword: 'password123',
 };
 
 describe('Authentication', () => {
@@ -40,24 +39,8 @@ describe('Authentication', () => {
       expect(response.body.message).toBeDefined();
     });
 
-    it('should return 409 Username already in use', async () => {
-      await request(server).post('/api/v1/auth/signup').send(newUserData);
-      const response = await request(server).post('/api/v1/auth/signup').send({
-        name: newUserData.name,
-        email: 'newEmail@gmail.com',
-        username: newUserData.username,
-        password: newUserData.password,
-        confirmPassword: newUserData.confirmPassword,
-      });
-
-      expect(response.status).toBe(409);
-      expect(response.body.message).toBeDefined();
-    });
-
     it('fails signup without required fields', async () => {
-      const response = await request(server)
-        .post('/api/v1/auth/signup')
-        .send({ name: newUserData.name, email: newUserData.email });
+      const response = await request(server).post('/api/v1/auth/signup').send({ firstName: newUserData.firstName, email: newUserData.email });
 
       expect(response.statusCode).toBe(400);
       expect(response.body.errors).toBeDefined();
@@ -65,42 +48,25 @@ describe('Authentication', () => {
   });
 
   describe('POST /api/v1/auth/login', () => {
-    it('can log in with username', async () => {
-      await request(server).post('/api/v1/auth/signup').send(newUserData);
-      const response = await request(server)
-        .post('/api/v1/auth/login')
-        .send({ emailOrUsername: newUserData.username, password: newUserData.password });
-
-      expect(response.status).toBe(200);
-      expect(response.body.data).toBeDefined();
-      expect(response.body.token).toBeDefined();
-    });
-
     it('can log in with email', async () => {
       await request(server).post('/api/v1/auth/signup').send(newUserData);
-      const response = await request(server)
-        .post('/api/v1/auth/login')
-        .send({ emailOrUsername: newUserData.email, password: newUserData.password });
+      const response = await request(server).post('/api/v1/auth/login').send({ email: newUserData.email, password: newUserData.password });
 
       expect(response.status).toBe(200);
       expect(response.body.data).toBeDefined();
       expect(response.body.token).toBeDefined();
     });
 
-    it('should return 401 Incorrect (email | username) or password`', async () => {
+    it('should return 401 Incorrect email or password`', async () => {
       await request(server).post('/api/v1/auth/signup').send(newUserData);
-      const response = await request(server)
-        .post('/api/v1/auth/login')
-        .send({ emailOrUsername: newUserData.email, password: 'wrongPassword' });
+      const response = await request(server).post('/api/v1/auth/login').send({ email: newUserData.email, password: 'wrongPassword' });
 
       expect(response.status).toBe(401);
       expect(response.body.message).toBeDefined();
     });
 
     it('fails login without required fields', async () => {
-      const response = await request(server)
-        .post('/api/v1/auth/signup')
-        .send({ name: newUserData.name, email: newUserData.email });
+      const response = await request(server).post('/api/v1/auth/signup').send({ name: newUserData.firstName, email: newUserData.email });
 
       expect(response.statusCode).toBe(400);
       expect(response.body.errors).toBeDefined();
