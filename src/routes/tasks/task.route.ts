19236d79d1cc6d03a250dcb4ee6356269e13cbc6
Ahmed Mostafa <ahmed.elasiri1@gmail.com>
2023-11-29 21:46:35 +0200

    Refactor TaskRoute class

--- src/routes/tasks/task.route.ts
+++ src/routes/tasks/task.route.ts
@@ -1,14 +1,15 @@
 import { Router } from 'express';
+import { autoInjectable } from 'tsyringe';
 
+import { TaskController } from '../../controllers/tasks/task.controller';
 import { Routes } from '../../interfaces';
 import { authenticateUser } from '../../middleware/auth.middleware';
-import { autoInjectable } from 'tsyringe';
-import {TaskController} from '../../controllers/tasks/task.controller';
+
+@autoInjectable()
 class TaskRoute implements Routes {
   public path = '/task';
   public router = Router();
 
-  @autoInjectable()
   constructor(private readonly taskController: TaskController) {
     this.initializerRoutes();
   }
@@ -16,20 +17,19 @@ class TaskRoute implements Routes {
     //### tasks routes that don't require authentication
     this.router.get(`${this.path}`, this.taskController.getTasks);
     this.router.get(`${this.path}/:id`, this.taskController.getTaskById);
-    this.router.get(`${this.path}/:id/offers`, this.taskController.getTaskOffers);
-
-    // this.router.get(`${this.path}/:id/images`, this.taskController.getTaskImages);
-    // this.router.get(`${this.path}/:id/owner`, this.taskController.getTaskOwner);
-    // this.router.get(`${this.path}/:id/tasker`, this.taskController.getTaskTasker);
+    // this.router.get(`${this.path}/:id/offers`, this.taskController.getTaskOffers);
 
     //### tasks routes that require authentication
     this.router.use(`${this.path}`, authenticateUser);
     this.router.post(`${this.path}`, this.taskController.createTask);
     this.router.put(`${this.path}/:id`, this.taskController.updateTask);
     this.router.delete(`${this.path}/:id`, this.taskController.deleteTask);
-    
-    // this.router.get(`${this.path}/:id/chat`, this.taskController.getTaskChat);
   }
 }
 
 export default TaskRoute;
+
+// this.router.get(`${this.path}/:id/images`, this.taskController.getTaskImages);
+// this.router.get(`${this.path}/:id/owner`, this.taskController.getTaskOwner);
+// this.router.get(`${this.path}/:id/tasker`, this.taskController.getTaskTasker);
+// this.router.get(`${this.path}/:id/chat`, this.taskController.getTaskChat);
