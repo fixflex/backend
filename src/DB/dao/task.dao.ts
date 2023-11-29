09e866119fca350f660d0e73a5d914960b8672de
Ahmed Mostafa <ahmed.elasiri1@gmail.com>
2023-11-29 21:47:25 +0200

    Add TaskDao class to handle task data access

---
+++ src/DB/dao/task.dao.ts
@@ -0,0 +1,11 @@
+import { ITask } from '../../interfaces';
+import TaskModel from '../models/task/task.model';
+import CommonDAO from './commonDAO';
+
+class TaskDao extends CommonDAO<ITask> {
+  constructor() {
+    super(TaskModel);
+  }
+}
+
+export { TaskDao };
