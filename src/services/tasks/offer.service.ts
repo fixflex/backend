14ed6b06731ce714f482f4ab526a767ea0f18fe9
Ahmed Mostafa <ahmed.elasiri1@gmail.com>
2023-11-30 04:08:20 +0200

    Refactor OfferService to check task status before creating an offer
    - Refactored OfferService to include TaskDao dependency
    - Added check to ensure current user is a tasker
    - Added check to ensure task exists
    - Added check to ensure task status is open before creating an offer

--- src/services/tasks/offer.service.ts
+++ src/services/tasks/offer.service.ts
@@ -1,17 +1,23 @@
 import { autoInjectable } from 'tsyringe';
 
 import { OfferDao } from '../../DB/dao/offer.dao';
+import { TaskDao } from '../../DB/dao/task.dao';
 import TaskerDao from '../../DB/dao/tasker.dao';
 import HttpException from '../../exceptions/HttpException';
+import { TaskStatus } from '../../interfaces/task.interface';
 
 @autoInjectable()
 class OfferService {
-  constructor(private offerDao: OfferDao, private taskerDao: TaskerDao) {}
+  constructor(private offerDao: OfferDao, private taskerDao: TaskerDao, private taskDao: TaskDao) {}
 
   async createOffer(offer: any, userId: string | undefined) {
-    // check if this user is a tasker
+    // check if the current user is a tasker
     let tasker = await this.taskerDao.getOne({ userId });
     if (!tasker) throw new HttpException(400, 'You are not a tasker');
+    // check if task exists
+    let task = await this.taskDao.getOneById(offer.taskId);
+    if (!task) throw new HttpException(400, 'Task not found');
+    if (task.status !== TaskStatus.OPEN) throw new HttpException(400, 'This task is not open for offers');
     offer.taskerId = tasker._id;
     return await this.offerDao.create(offer);
   }
