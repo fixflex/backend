45762b8581e9f02b7fe66d3c2bc42cdaff5fdd7d
Ahmed Mostafa <ahmed.elasiri1@gmail.com>
2023-12-03 04:49:28 +0200

    Refactor image upload middleware
    This commit refactors the image upload middleware in the application. The multer options function has been moved to the bottom of the file for better readability. The filename generation logic has been updated to include 'cover' in the filename if the file fieldname is 'imageCover'. This makes it easier to identify cover images in the uploads directory. The rest of the changes are minor code cleanups and reordering for better readability and maintainability.

--- src/middleware/uploadImages.middleware.ts
+++ src/middleware/uploadImages.middleware.ts
@@ -3,23 +3,17 @@ import multer, { FileFilterCallback } from 'multer';
 
 import HttpException from '../exceptions/HttpException';
 
-const multerOptions = () =>
-  multer({
-    storage: multerStorage,
-    fileFilter: multerFilter,
-    limits: { fileSize: 1024 * 1024 * 2 }, // 2MB
-  });
-
 // TODO: use memory storage instead of disk storage
 const multerStorage = multer.diskStorage({
   destination: (_req, _file, cb) => {
     cb(null, `${process.cwd()}/uploads`);
   },
   filename: (_req, file, cb) => {
+    let imageFileName: string;
     const ext = file.mimetype.split('/')[1];
     // it the file is imageCover then the name will contain cover
-    // if (file.fieldname === 'profilePicture') return cb(null, `profilePicture-${new Date().toISOString().replace(/:/g, '-')}.${ext}`);
-    const imageFileName = `${new Date().toISOString().replace(/:/g, '-')}.${ext}`;
+    if (file.fieldname === 'imageCover') imageFileName = `cover-${new Date().toISOString().replace(/:/g, '-')}.${ext}`;
+    else imageFileName = `${new Date().toISOString().replace(/:/g, '-')}.${ext}`;
     cb(null, imageFileName);
   },
 });
@@ -31,6 +25,12 @@ const multerFilter = (_req: Request, file: Express.Multer.File, cd: FileFilterCa
     cd(new HttpException(400, 'Not an image! Please upload only images'));
   }
 };
+const multerOptions = () =>
+  multer({
+    storage: multerStorage,
+    fileFilter: multerFilter,
+    limits: { fileSize: 1024 * 1024 * 2 }, // 2MB
+  });
 
 export const uploadSingleFile = (fileName: string) => multerOptions().single(fileName);
 export const uploadMixFiles = (arrayOfFields: { name: string; maxCount?: number }[]) => multerOptions().fields(arrayOfFields);
