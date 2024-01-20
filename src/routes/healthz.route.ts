b3801dc2104ec5bdaf2603358238331d8cb25b6d
Ahmed Mostafa <ahmed.elasiri1@gmail.com>
2024-01-20 07:55:53 +0200

    Refactor HealthzRoute initializerRoutes method
    Remove commented code and unused variables in the HealthzRoute initializerRoutes method.

--- src/routes/healthz.route.ts
+++ src/routes/healthz.route.ts
@@ -13,30 +13,10 @@ class HealthzRoute implements Routes {
   }
   private initializerRoutes() {
     this.router.get(`${this.path}`, (_req: Request, res: Response) => {
-      // console.log(_req.cookies.healthz);
       res.status(200).json(customResponse({ data: null, success: true, status: 200, message: 'Welcome to Rest API - 👋🌎🌍🌏', error: false }));
     });
 
     this.router.get('/', (_req: Request, res: Response) => {
-      // log request cookies
-      // console.log('Cookies: ', _req.cookies);
-      // res.cookie('TestAccessToken', 'token', {
-      //   httpOnly: true, // client side js cannot access the cookie
-      //   maxAge: 24 * 60 * 60 * 1000, // one days
-      //   secure: process.env.NODE_ENV === 'production', // cookie only works in https
-      //   // privent cross-site access to the cookie (only allow same site access)
-      //   sameSite: 'strict', // cross-site access not allowed
-      // });
-      // console.log(_req.cookies.healthz);
-
-      // res.cookie('healthz', 'token_healthz', {
-      //   // httpOnly: true, // client side js cannot access the cookie
-      //   // maxAge: 24 * 60 * 60 * 1000, // one days
-      //   // secure: process.env.NODE_ENV === 'production', // cookie only works in https
-      //   secure: false, // cookie only works in https
-      //   // sameSite: 'lax', // cross-site access not allowed
-      //   path: '/api/v1/healthz',
-      // });
       res.status(200).json(customResponse({ data: null, success: true, status: 200, message: 'Welcome to Rest API - 👋🌎🌍🌏', error: false }));
     });
   }
