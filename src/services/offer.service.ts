6f38f80da86a8895d357c67be03b09fdea44b5ba
Ahmed Mostafa <ahmed.elasiri1@gmail.com>
2024-01-30 17:25:05 +0200

    Refactor createOffer method and add taskerId to offer

--- src/services/offer.service.ts
+++ src/services/offer.service.ts
@@ -11,6 +11,19 @@ import { TaskStatus } from '../interfaces/task.interface';
 class OfferService implements IOfferService {
   constructor(private offerDao: OfferDao, private taskerDao: TaskerDao, private taskDao: TaskDao) {}
 
+  // // check if the current user is a tasker
+  // let tasker = await this.taskerDao.getOne({ userId });
+  // if (!tasker) throw new HttpException(400, 'You are not a tasker');
+  // // check if task exists
+  // let task = await this.taskDao.getOneById(offer.taskId);
+  // if (!task) throw new HttpException(400, 'Task not found');
+  // if (task.status !== TaskStatus.OPEN) throw new HttpException(400, 'This task is not open for offers');
+  // offer.taskerId = tasker._id;
+  // let createdOffer = await this.offerDao.create(offer);
+  // // update the task status to assigned and add the offer id to the task offers array
+  // await this.taskDao.updateOneById(offer.taskId, {
+  //   $push: { offers: createdOffer._id },
+  // });
   async createOffer(offer: IOffer, userId: string) {
     // 1. check if the user is a tasker
     let tasker = await this.taskerDao.getOne({ userId });
@@ -22,7 +35,8 @@ class OfferService implements IOfferService {
     // 3. check if the tasker already made an offer on this task, if yes return an error
     let isOfferExist = await this.offerDao.getOne({ taskId: offer.taskId, taskerId: tasker._id });
     if (isOfferExist) throw new HttpException(400, 'something_went_wrong');
-    // 4. create the offer
+    // 4. create the offer and add the tasker id to it
+    offer.taskerId = tasker._id;
     let newOffer = await this.offerDao.create(offer);
     // 5. update the task offers array with the new offer
     await this.taskDao.updateOneById(offer.taskId, { $push: { offers: newOffer._id } });
