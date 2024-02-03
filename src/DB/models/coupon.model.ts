df92a262068023a008df49443d266db4a30ef205
Ahmed Mostafa <ahmed.elasiri1@gmail.com>
2024-02-03 13:55:28 +0200

    Add coupon model to DB

---
+++ src/DB/models/coupon.model.ts
@@ -0,0 +1,47 @@
+import { Schema, model } from 'mongoose';
+
+import { CouponType, ICoupon } from '../../interfaces';
+
+const couponSchema = new Schema<ICoupon>(
+  {
+    code: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: Object.values(CouponType),
+      default: CouponType.PERCENTAGE,
+    },
+    value: {
+      type: Number,
+      required: true,
+    },
+    expirationDate: {
+      type: Date,
+    },
+    maxUses: {
+      type: Number,
+      required: true,
+    },
+    remainingUses: {
+      type: Number,
+      required: true,
+    },
+    minOrderAmount: {
+      type: Number,
+    },
+    createdBy: {
+      // the admin who created the coupon
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Coupon = model<ICoupon>('Coupon', couponSchema);
+
+export { Coupon };
