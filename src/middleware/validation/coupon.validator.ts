c3392433156fb9c17291cc3a5684b4269bb5be0d
Ahmed Mostafa <ahmed.elasiri1@gmail.com>
2024-02-03 13:54:55 +0200

    Add coupon validation middleware

---
+++ src/middleware/validation/coupon.validator.ts
@@ -0,0 +1,32 @@
+import { check } from 'express-validator';
+
+import { CouponType } from '../../interfaces';
+import validatorMiddleware from '../errors/validation.middleware';
+
+export const createCouponValidator = [
+  check('code').notEmpty().withMessage('code is required').isString().withMessage('code must be a string'),
+  check('type').optional().isIn(Object.values(CouponType)).withMessage('invalid coupon type'),
+  check('value')
+    .notEmpty()
+    .withMessage('value is required')
+    .isNumeric()
+    .withMessage('value must be a number')
+    .custom(value => {
+      if (value < 0) {
+        throw new Error('value must be positive');
+      }
+      return true;
+    }),
+  check('maxUses', 'maxUses is required')
+    .notEmpty()
+    .isNumeric()
+    .withMessage('maxUses must be a number')
+    .custom(maxUses => {
+      if (maxUses < 0) {
+        throw new Error('maxUses must be positive');
+      }
+      return true;
+    }),
+
+  validatorMiddleware,
+];
