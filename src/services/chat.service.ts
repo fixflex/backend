46d4d03f3287cbb6bc19f6081dcd70948009f69c
Ahmed Mostafa <ahmed.elasiri1@gmail.com>
2024-02-12 21:28:18 +0200

    Refactor ChatService createChat method
    - Import necessary dependencies and modules
    - Add constructor to inject ChatDao dependency
    - Update createChat method to include data and user parameters
    - Implement logic to check if user and tasker are the same and if they exist
    - Implement logic to check if chat already exists
    - Create new chat if it doesn't exist and emit event to user and tasker
    - Return the new chat

--- src/services/chat.service.ts
+++ src/services/chat.service.ts
@@ -1,7 +1,13 @@
-import { ChatDao } from '../DB/dao/chat.dao';
-import { IChatService } from '../interfaces';
+import { autoInjectable } from 'tsyringe';
 
+import { io } from '..';
+import { ChatDao } from '../DB/dao/chat.dao';
+import HttpException from '../exceptions/HttpException';
+import { IChat, IChatService, IUser } from '../interfaces';
+
+@autoInjectable()
 class ChatService implements IChatService {
+  constructor(private chatDao: ChatDao) {}
   async getChatById(id: string) {
     return await ChatDao.getChatById(id);
   }
@@ -10,8 +16,20 @@ class ChatService implements IChatService {
     return await ChatDao.getChatByUserId(id);
   }
 
-  async createChat(data: any) {
-    return await ChatDao.createChat(data);
+  async createChat(data: IChat, user: IUser) {
+    // 1. check that the user and tasker are not the same && that they exist && user === req.user._id
+    if (data.user === data.tasker) throw new HttpException(400, 'User and tasker cannot be the same');
+    if (data.user !== user._id) throw new HttpException(403, 'Unauthorized');
+    // 2. check if chat already  exists
+    let chat = await this.chatDao.getOne({ user: data.user, tasker: data.tasker });
+    if (chat) return chat;
+    // 3. create chat and return it
+    let newChat = await this.chatDao.create(data);
+    // make user and tasker join the chat room where the room name is the newChat._id
+    io.to(newChat.user).emit('newChatRoom', newChat);
+    io.to(newChat.tasker).emit('newChatRoom', newChat);
+
+    return newChat;
   }
 }
 
