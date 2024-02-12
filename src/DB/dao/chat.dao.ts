c46524e156cd080c9ba2f3aa51fef09e6dcfab21
Ahmed Mostafa <ahmed.elasiri1@gmail.com>
2024-02-12 22:42:00 +0200

    Refactor chat.dao.ts and chat.validator.ts

--- src/DB/dao/chat.dao.ts
+++ src/DB/dao/chat.dao.ts
@@ -6,20 +6,20 @@ class ChatDao extends BaseDao<IChat> {
   constructor() {
     super(ChatModel);
   }
-  static async getChatById(id: string) {
-    return await ChatModel.findById(id).populate('messages');
-  }
+  // static async getChatById(id: string) {
+  //   return await ChatModel.findById(id).populate('messages');
+  // }
 
-  static async getChatByUserId(id: string) {
-    let chats = await ChatModel.find({ $or: [{ client: id }, { tasker: id }] }, 'client tasker');
+  // static async getChatByUserId(id: string) {
+  //   let chats = await ChatModel.find({ $or: [{ client: id }, { tasker: id }] }, 'client tasker');
 
-    // console.log(chats);
-    return chats;
-  }
+  //   // console.log(chats);
+  //   return chats;
+  // }
 
-  static async createChat(data: any) {
-    return await ChatModel.create(data);
-  }
+  // static async createChat(data: any) {
+  //   return await ChatModel.create(data);
+  // }
 }
 
 export { ChatDao };
--- src/middleware/validation/chat.validator.ts
+++ src/middleware/validation/chat.validator.ts
@@ -3,11 +3,13 @@ import { check } from 'express-validator';
 import validatorMiddleware from '../errors/validation.middleware';
 
 export const createChatValidator = [
-  check('user').isMongoId().withMessage('invalid_MongoId'),
   check('tasker').isMongoId().withMessage('invalid_MongoId'),
 
   // ====================>>>>>>>> is empty <<<<<<<<<<<==================== //
   check('_id').isEmpty().withMessage('not_allowed'),
   check('messages').isEmpty().withMessage('not_allowed'),
+  check('user').isEmpty().withMessage('not_allowed'),
+  check('createdAt').isEmpty().withMessage('not_allowed'),
+  check('updatedAt').isEmpty().withMessage('not_allowed'),
   validatorMiddleware,
 ];
