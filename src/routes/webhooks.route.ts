d1a2f16d84526a21f2e45a24eb74488c9ea74451
Ahmed Mostafa <ahmed.elasiri1@gmail.com>
2024-02-28 09:44:07 +0200

    Add asyncHandler to handle transaction webhook

--- src/routes/webhooks.route.ts
+++ src/routes/webhooks.route.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import asyncHandler from 'express-async-handler';
 import { autoInjectable } from 'tsyringe';
 
 import { PaymobService } from '../services/paymob.service';
@@ -13,12 +14,18 @@ class WebhooksRoute {
   }
 
   private initializeRoutes() {
-    this.router.post(`${this.path}/paymob`, async (req, res) => {
-      let results = await this.paymobService.handleTransactionWebhook(req.body.obj, req.query.hmac);
-      res.status(200).json({ results });
-    });
-
-    this.router.get(`${this.path}/paymob`, async (req, res) => {
+    this.router.post(
+      `${this.path}/paymob`,
+      asyncHandler(async (req, res) => {
+        let results;
+        if (!req.body.obj.is_voided && !req.body.obj.is_refunded) {
+          // console.log('webhook ======++++++>', req.body.obj);
+          results = await this.paymobService.handleTransactionWebhook(req.body.obj, req.query.hmac);
+        }
+        res.status(200).json({ results });
+      })
+    );
+    this.router.get(`${this.path}/paymob/success`, async (req, res) => {
       res.status(200).json({ message: 'success', data: req.query });
     });
   }
