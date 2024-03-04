9877d5ec8e94214d7f69927169e303ca423b0e0e
Ahmed Mostafa <ahmed.elasiri1@gmail.com>
2024-03-04 09:48:56 +0200

    Delete unused files and dependencies

--- dist/interfaces/tasker.interface.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// export interface ITaskerRepository {
-//   getTasker(taskerId: string): Promise<ITasker | null>;
-//   getTaskers(): Promise<ITasker[]>;
-//   createTasker(tasker: ITasker): Promise<ITasker>;
-//   updateTasker(taskerId: string, tasker: Partial<ITasker>): Promise<ITasker | null>;
-//   deleteTasker(taskerId: string): Promise<ITasker | null>;
-// }
--- dist/interfaces/user.interface.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserType = void 0;
-var UserType;
-(function (UserType) {
-    UserType["USER"] = "USER";
-    UserType["ADMIN"] = "ADMIN";
-})(UserType || (exports.UserType = UserType = {}));
