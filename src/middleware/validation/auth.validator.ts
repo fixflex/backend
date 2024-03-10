e768f4979a05ee1faee27636e593958196ec5c0d
Ahmed Mostafa <ahmed.elasiri1@gmail.com>
2024-03-10 11:28:03 +0200

    Refactor phone number validation in auth and user validators

--- src/middleware/validation/auth.validator.ts
+++ src/middleware/validation/auth.validator.ts
@@ -10,8 +10,8 @@ export const signupValidator = [
   check('email').notEmpty().withMessage('is_required').isEmail().withMessage('invalid_email'),
 
   check('password').notEmpty().withMessage('is_required').isLength({ min: 8 }).withMessage('invalid_password'),
-  check('phoneNumber').notEmpty().withMessage('this_field_is_required').isMobilePhone('ar-EG').withMessage('invalid_phone_number'),
-
+  // check('phoneNumber').notEmpty().withMessage('this_field_is_required').isMobilePhone('ar-EG').withMessage('invalid_phone_number'),
+  check('phoneNumber').optional().isMobilePhone('ar-EG').withMessage('invalid_phone_number'),
   check('role').isEmpty().withMessage('not_allowed'), // 'role is not allowed
   check('active').isEmpty().withMessage('not_allowed'),
   validatorMiddleware,
--- src/middleware/validation/user.validator.ts
+++ src/middleware/validation/user.validator.ts
@@ -6,8 +6,8 @@ export const updateLoggedUserValidator = [
   check('name').optional().isString().withMessage('invalid_input'),
   check('email').optional().isEmail().withMessage('invalid_email'),
   check('password').isEmpty().withMessage('not_allowed'),
-  check('phoneNumber').notEmpty().withMessage('this_field_is_required').isMobilePhone('ar-EG').withMessage('invalid_phone_number'),
-
+  // check('phoneNumber').notEmpty().withMessage('this_field_is_required').isMobilePhone('ar-EG').withMessage('invalid_phone_number'),
+  check('phoneNumber').optional().isMobilePhone('ar-EG').withMessage('invalid_phone_number'),
   // check('location')
   //   .optional()
   //   .isArray()
